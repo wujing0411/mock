c937a01b8c7de9188168174cca8b7fa9f18f8817
wujing0411 <wuj_lilo@live.com>
2017-11-22 18:40:31 +0800

    Update sign.js

--- nodejs-mock-server/sign.js
+++ nodejs-mock-server/sign.js
@@ -4,7 +4,7 @@
 module.exports={
 	host: 'http://127.0.0.1',
     port: '8888',
-    path:'/accounting/sign/query/4BCA4460-BC93-E711-8C05-48D539B907D4',
-    acctIp:'http://118.178.192.25:8120',
+    path:'/test',
+    acctIp:'http://trueIp:port',
     responseBody:'{"1":{id:1,name:"test1"},"2":{id:2,name:"test2"}}'
 };
